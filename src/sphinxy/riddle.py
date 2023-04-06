322ed50839193146c72acaf1d11fc609352fa9eb
Olga Matoula <olgamatoula@gmail.com>
2023-04-06 19:32:51 +0300

    Add docstrings

--- src/sphinxy/riddle.py
+++ src/sphinxy/riddle.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Iterator
 
 
 @dataclass(frozen=True)
@@ -7,7 +8,21 @@ class Riddle:
     answer: str
 
     def check_answer(self, answer: str) -> bool:
+        """Evaluates given answer against the actual riddle answer.
+
+        Args:
+            answer (str): The answer to the riddle question.
+
+        Returns:
+            bool: The result of the evaluation; True if answer is correct, False otherwise.
+        """
         return answer.lower() == self.answer.lower()
 
-    def get_hint(self) -> str:
-        return self.answer[0]
+    def get_hint(self) -> Iterator[str]:
+        """Gives the next letter of the answer as a hint to the question
+
+        Yields:
+            Iterator[str]: The next character of the answer.
+        """
+
+        yield from iter(self.answer)
--- src/sphinxy/sphinx.py
+++ src/sphinxy/sphinx.py
@@ -2,11 +2,18 @@ from sphinxy.riddle import Riddle
 
 
 class IncorrectAnswer(Exception):
+    """Exception to indicate an incorrect answer."""
+
     ...
 
 
 class Sphinx:
     def __init__(self, name: str):
+        """A Sphinx guards the gates of Thebes and asks riddles to strangers who approach.
+
+        Args:
+            name (str): The name of the Sphinx.
+        """
         self._name = name
         self._riddle = Riddle(
             question=(
@@ -17,22 +24,59 @@ class Sphinx:
         )
 
     def introduce(self) -> str:
+        """The Sphinx introduces itself.
+
+        Returns:
+            str: The introduction speech.
+        """
         return (
             f"Greetings, mortals. I am {self._name}. I have guarded the city of Thebes"
             "for centuries and posed riddles to those who dared to approach me."
         )
 
     def update_riddle(self, riddle: Riddle) -> str:
+        """Updates stored riddle.
+
+        Args:
+            riddle (Riddle): The new riddle to store.
+
+        Returns:
+            str: A confirmation that the riddle has been updated.
+        """
         self._riddle = riddle
         return "I have updated my riddle. Are you ready to solve it?"
 
-    def pose_riddle(self, include_hint: bool = False) -> tuple[str, str]:
-        hint = ""
-        if include_hint:
-            hint = f"Hint: The answer starts with the letter '{self._riddle.get_hint()}'."
+    def pose_riddle(self, include_hint: bool = False) -> tuple[str, str | None]:
+        """Poses the stored riddle and a hint if requested.
+
+        Args:
+            include_hint (bool, optional): Set to True to ask for hint. Defaults to False.
+
+        Returns:
+            tuple[str, str | None]: The riddle and a hint or an empty string or None
+                if no hint was requested.
+        """
+        hint = (
+            f"Hint: The answer starts with the letter '{self._riddle.get_hint()}'."
+            if include_hint
+            else None
+        )
         return (self._riddle.question, hint)
 
     def check_riddle_answer(self, answer: str, return_hint: bool = False) -> str:
+        """Evaluates the given answer to the riddle.
+
+        Args:
+            answer (str): The given answer to the riddle.
+            return_hint (bool, optional): Controls whether a hint for the riddle should be returned.
+                Defaults to False.
+
+        Raises:
+            IncorrectAnswer: Exception for incorrect answer.
+
+        Returns:
+            str: The result of the evaluation of the answer.
+        """
         if self._riddle.check_answer(answer):
             return "Your answer was correct. You may pass."
         elif return_hint:
@@ -45,4 +89,12 @@ class Sphinx:
 
     @staticmethod
     def sleep(duration: int = 5) -> str:
+        """Puts the Sphinx to sleep for requested duration.
+
+        Args:
+            duration (int, optional): Sleep duration in years. Defaults to 5.
+
+        Returns:
+            str: A confirmation that the Sphinx will sleep for the requested period.
+        """
         return f"I am tired of waiting. I will sleep for {duration} years."
