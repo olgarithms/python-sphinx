a4a6598d7407fdb7a9c68ad2f633c1591ac727ec
Aya Elsayed <aelsayed12@bloomberg.net>
2023-04-10 12:26:38 +0100

    Documenting your API

--- src/sphinxy/riddle.py
+++ src/sphinxy/riddle.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Iterator
 
 
 @dataclass(frozen=True)
@@ -7,7 +8,20 @@ class Riddle:
     answer: str
 
     def check_answer(self, answer: str) -> bool:
+        """Evaluates given answer against the riddle answer
+
+        Args:
+            answer (str): Riddle answer under test
+
+        Returns:
+            bool: True if answer is correct, False otherwise
+        """
         return answer.lower() == self.answer.lower()
 
-    def get_hint(self) -> str:
-        return self.answer[0]
+    def get_hint(self) -> Iterator[str]:
+        """Gives a hint for the answer
+
+        Yields:
+            Iterator[str]: the answer, revealed one letter at a time
+        """
+        yield from iter(self.answer)
--- src/sphinxy/sphinx.py
+++ src/sphinxy/sphinx.py
@@ -2,6 +2,8 @@ from sphinxy.riddle import Riddle
 
 
 class IncorrectAnswer(Exception):
+    """Use this exception to indicate an incorrect answer."""
+
     ...
 
 
@@ -17,22 +19,59 @@ class Sphinx:
         )
 
     def introduce(self) -> str:
+        """The Sphinx introduces itself
+
+        Returns:
+            str: A string introduction
+        """
         return (
             f"Greetings, mortals. I am {self._name}. I have guarded the city of Thebes"
             "for centuries and posed riddles to those who dared to approach me."
         )
 
     def update_riddle(self, riddle: Riddle) -> str:
+        """Updates stored riddle
+
+        Args:
+            riddle (Riddle): The new riddle to store
+
+        Returns:
+            str: A confirmation that the riddle has been updated
+        """
         self._riddle = riddle
         return "I have updated my riddle. Are you ready to solve it?"
 
     def pose_riddle(self, include_hint: bool = False) -> tuple[str, str]:
+        """Poses the stored riddle and a hint, if requested.
+
+        Args:
+            include_hint (bool, optional): Set to True to ask for hint. Defaults to False.
+
+        Returns:
+            tuple[str, str]: The riddle and a hint or an empty string if no hint requested
+        """
         hint = ""
         if include_hint:
-            hint = f"Hint: The answer starts with the letter '{self._riddle.get_hint()}'."
+            hint = (
+                f"Hint: The answer starts with the letter '{self._riddle.get_hint()}'."
+            )
         return (self._riddle.question, hint)
 
     def check_riddle_answer(self, answer: str, return_hint: bool = False) -> str:
+        """Evaluates the answer given for he riddle.
+
+        Args:
+            answer (str): The given answer to the riddle.
+            return_hint (bool, optional): Controls whether a hint for the riddle should be returned.
+                Defaults to False.
+
+        Raises:
+            IncorrectAnswer: Exception for incorrect answer.
+
+        Returns:
+            str: The result of the evaluation of the answer.
+        """
+
         if self._riddle.check_answer(answer):
             return "Your answer was correct. You may pass."
         elif return_hint:
@@ -45,4 +84,12 @@ class Sphinx:
 
     @staticmethod
     def sleep(duration: int = 5) -> str:
+        """Puts the Sphinx to sleep for requested durationn
+
+        Args:
+            duration (int, optional): Sleep duration in years. Defaults to 5.
+
+        Returns:
+            str: A confirmation that the Sphinx will sleep for the requested period
+        """
         return f"I am tired of waiting. I will sleep for {duration} years."
